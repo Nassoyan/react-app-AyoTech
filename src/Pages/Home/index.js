b454b929e1f58195afe93a094fe41139b4fd81da
NikoghosyanJan <nikoghosyanjan10@gmail.com>
2023-01-11 21:57:50 -0800

    added homepageHeader

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,11 @@
+import HomePageHeader from "../../Components/HomPageHeader"
+
+function Home() {
+    return (
+        <div>
+            <HomePageHeader />
+        </div>
+    )
+}
+
+export default Home
